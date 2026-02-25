9c43679ecd0658ccb77c5df4fa87d925ace9b4b1
Tr1kst3r-cloud <db132383@gmail.com>
2026-02-25 04:34:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require("discord.js");
+const express = require("express");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servidor web obrigatório para o Render
+app.get("/", (req, res) => {
+  res.send("Bot online 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
+
+// Configuração do bot
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -8,14 +22,17 @@ const client = new Client({
   ]
 });
 
-client.once('ready', () => {
-  console.log(`Bot online como ${client.user.tag}`);
+// Quando o bot ligar
+client.once("ready", () => {
+  console.log(`Bot logado como ${client.user.tag}`);
 });
 
-client.on('messageCreate', message => {
-  if (message.content === '!ping') {
-    message.reply('🏓 Pong! A Complexo Store está online!');
+// Comando simples
+client.on("messageCreate", (message) => {
+  if (message.content === "!ping") {
+    message.reply("Pong 🏓");
   }
 });
 
-client.login(process.env.TOKEN);
+// Login com variável de ambiente (IMPORTANTE)
+client.login(process.env.DISCORD_TOKEN);
